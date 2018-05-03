f1cb504095924a55184bed1af16ea175b2be8d47
Dan Kolbman <dankolbman@gmail.com>
2018-05-03 10:38:53 -0400

    get status of dataservice

--- README.md
+++ README.md
@@ -2,3 +2,14 @@ Kids First File Registry Lambda
 ===============================
 
 A lambda to listen for new files and import them to the Data Service
+
+
+# Testing
+
+The function may be tested using sam local.
+This will create an event and invoke it against the function. The function will
+make calls to live aws s3 and dataservice, so ensure that those services
+are accessible.
+```
+sam local generate-event s3 --bucket kf-study-us-east-1-dev-sd-9pyzahhe --key harmonized/cram/BS_H14CBVAQ.cram  | DATASERVICE_API=http://kf-api-dataservice-dev.kids-first.io sam local invoke fileregistry -t template.yml
+```
--- service.py
+++ service.py
@@ -1,6 +1,6 @@
 import os
 import boto3
-import requests
+from botocore.vendored import requests
 
 
 s3 = boto3.client("s3")
@@ -10,10 +10,10 @@ DATA_TYPES = {
     'fq': 'Unaligned Reads',
     'fasta': 'Unaligned Reads',
     'fastq': 'Unaligned Reads',
-    'bam': 'Aligned Reads',
-    'cram': 'Aligned Reads',
-    'bai': 'Read Index',
-    'crai': 'Read Index',
+    'bam': 'Submitted Aligned Reads',
+    'cram': 'Submitted Aligned Reads',
+    'bai': 'Submitted Aligned Reads Index',
+    'crai': 'Submitted Aligned Reads Index',
     'vcf': 'Population Variant Calls',
     'gvcf': 'Individual Variant Calls'
 }
@@ -25,37 +25,94 @@ def handler(event, context):
     """
     DATASERVICE_API = os.environ.get('DATASERVICE_API', None)
     if DATASERVICE_API is None:
-        return
+        return 'no dataservice url set'
 
+    res = {}
     for record in event['Records']:
-        bucket = record['s3']['bucket']['name']
-        key = record['s3']['object']['key'] 
-        version = record['s3']['object']['versionId'] 
-        
-        tags = s3.get_object_tagging(Bucket=bucket, Key=key)
-        tags = {t['Key']: t['Value'] for t in tags['TagSet']}
+        r = process_record(DATASERVICE_API, record)
+        res[record['s3']['object']['key']] = r
 
+    return res
+
+def process_record(api, record):
+    bucket = record['s3']['bucket']['name']
+    key = record['s3']['object']['key']
+
+    tags = s3.get_object_tagging(Bucket=bucket, Key=key)
+    tags = {t['Key']: t['Value'] for t in tags['TagSet']}
+
+    # Skip if there is a kf_id assigned already and exists in dataservice
+    gf_id = None
+    if 'gf_id' in tags:
+        url = api+'genomic-files/'+tags['gf_id']
+        resp = requests.get(url)
+        if resp.status_code == 200:
+            return 'already imported'
+        # Save for later so we can import with pre-determined id
+        gf_id = tags['gf_id']
+
+    req_tags = ['cavatica_harmonized_file', 'cavatica_source_file',
+                'cavatica_app', 'bs_id', 'cavatica_source_path',
+                'cavatica_task']
+
+    # Make sure the required tags are there
+    for tag in req_tags:
+        if tag not in tags:
+            return 'missing required tag {}'.format(tag)
+
+    file_name = key.split('/')[-1]
+    hashes = {'etag': record['s3']['object']['eTag']}
+    size = record['s3']['object']['size']
+    urls = ['s3://{}/{}'.format(bucket, key)]
+    file_format = key.split('/')[-1].split('.')[1].lower()
+    data_type = DATA_TYPES[file_format.lower()]
+
+    gf = {
+        'file_name': file_name,
+        'file_format': file_format,
+        'data_type': data_type,
+        'availability': 'Immediate Download',
+        'controlled_access': True,
+        'is_harmonized': True,
+        'hashes': hashes,
+        'size': size,
+        'urls': urls
+    }
+    # There is already a kf_id tagged on the file, so use that
+    if gf_id is not None:
+        gf['kf_id'] = gf_id
+
+    resp = requests.post(api+'genomic-files', json=gf)
+
+    if ('results' not in resp.json() and
+        'kf_id' not in resp.json()['results']):
+        return 'bad response from dataservice'
+
+    # Update tags with gf_id
+    tags['gf_id'] = resp.json()['results']['kf_id']
+    tagset = {'TagSet': [{'Key': k, 'Value': v} for k, v in tags.items()]}
+    r = s3.put_object_tagging(Bucket=bucket, Key=key, Tagging=tagset)
+    return 'imported'
+
+
+def register_input(source_path):
+        bucket = source_path.split('_')[0]
+        key = '/'.join(source_path.split('/')[1:])
+        
         file_name = key.split('/')[-1]
-        harmonized = key.split('/')[0] == 'harmonized'
         hashes = {'etag': record['s3']['object']['eTag']}
         size = record['s3']['object']['size']
         urls = ['s3://{}/{}'.format(bucket, key)]
         file_format = key.split('/')[-1].split('.')[1].lower()
         data_type = DATA_TYPES[file_format.lower()]
 
-
         gf = {
             'file_name': file_name,
             'file_format': file_format,
             'data_type': data_type,
-            'availability': 'available',
+            'availability': 'Immediate Download',
             'controlled_access': True,
-            'harmonized': harmonized,
             'hashes': hashes,
             'size': size,
             'urls': urls
         }
-
-        resp = requests.post(DATASERVICE_API+'/genomic-files', json=gf)
-
-    return 'ok'
