211a00c44c3a4159d9d69ffb2eb56906c1f750cf
Dan Kolbman <dankolbman@gmail.com>
2018-05-11 09:46:54 -0400

    :recycle: Refactor service and add tests

--- service.py
+++ service.py
@@ -6,6 +6,7 @@ from base64 import b64decode
 
 s3 = boto3.client("s3")
 
+
 DATA_TYPES = {
     'fa': 'Unaligned Reads',
     'fq': 'Unaligned Reads',
@@ -19,11 +20,17 @@ DATA_TYPES = {
     'g.vcf.gz.tbi': 'Individual Variant Calls'
 }
 
-TOKEN = os.environ.get('CAVATICA_TOKEN', None)
-HEADERS = None
-if TOKEN:
-    CAVATICA_TOKEN = boto3.client('kms').decrypt(CiphertextBlob=b64decode(TOKEN))['Plaintext']
-    HEADERS = {'X-SBG-Auth-Token': CAVATICA_TOKEN}
+
+
+
+class ImportException(Exception):
+        pass
+
+class DataServiceException(Exception):
+        pass
+
+class CavaticaException(Exception):
+        pass
 
 
 def handler(event, context):
@@ -34,156 +41,190 @@ def handler(event, context):
     if DATASERVICE_API is None:
         return 'no dataservice url set'
 
+    TOKEN = os.environ.get('CAVATICA_TOKEN', None)
+    CAVATICA_TOKEN = None
+    if TOKEN:
+        CAVATICA_TOKEN = boto3.client('kms').decrypt(CiphertextBlob=b64decode(TOKEN)).get(['Plaintext'], None)
+        HEADERS = {'X-SBG-Auth-Token': CAVATICA_TOKEN}
+
+    importer = FileImporter(DATASERVICE_API, CAVATICA_TOKEN)
     res = {}
     for record in event['Records']:
-        r = process_record(DATASERVICE_API, record)
-        res[record['s3']['object']['key']] = r
-
+        bucket = record['s3']['bucket']['name']
+        key = record['s3']['object']['key']
+        name = '{}/{}'.format(bucket, key)
+        res[name] = importer.import_from_event(record)
     return res
 
 
-def check_gf_id(tags):
-    """
-    Checks if a tagset has a gf_id and if a genomic file with that id exists
-    in the dataservice
-    """
-    gf_id = None
-    if 'gf_id' in tags:
-        url = api+'genomic-files/'+tags['gf_id']
-        resp = requests.get(url)
-        if resp.status_code == 200:
-            return 'already imported'
-        # Save for later so we can import with pre-determined id
-        gf_id = tags['gf_id']
-    return gf_id
+class FileImporter:
+
+    def __init__(self, api, cavatica_token):
+        self.api = api
+        self.cavatica_token = cavatica_token
+
+    def import_from_event(self, event):
+        """
+        Processes a single record from an s3 event
+        """
+        res = {'harmonized': 'not imported', 'source': 'not imported'}
+        try:
+            tags = self.import_harmonized(event)
+            res['harmonized'] = 'imported'
+        except (DataServiceException, ImportException) as err:
+            res['harmonized'] = str(err)
+            return res
+            
+        try:
+            self.register_input(tags)
+            res['source'] = 'imported'
+        except (DataServiceException, ImportException) as err:
+            res['source'] = str(err)
+
+        return res
+
+    def import_harmonized(self, record):
+        """
+        Imports a harmonized file from an s3 event record
+
+        The object in the event must already be tagged with the following 
+        required fields in order to be imported:
+
+        - `cavatica_harmonized_file`
+        - `cavatica_source_file`
+        - `cavatica_app`
+        - `bs_id`
+        - `cavatica_source_path`
+        - `cavatica_task`
+
+        If there is a `gf_id` tag on the object already, check to see if that
+        file already exists in the dataservice, if it does not, assume that
+        it is a pre-determined kf_id and use it when creating a new genomic file.
+        
+        Once the genomic file has been imported to the dataservice, tag the object
+        with the kf_id under the `gf_id` tag, unless there was already a `gf_id`
+        field there.
+        """
+        bucket = record['s3']['bucket']['name']
+        key = record['s3']['object']['key']
+
+        tags = s3.get_object_tagging(Bucket=bucket, Key=key)
+        tags = {t['Key']: t['Value'] for t in tags['TagSet']}
+
+        # Skip if there is a kf_id assigned already and exists in dataservice
+        gf_id = self.get_gf_id_tag(tags)
+
+        req_tags = ['cavatica_harmonized_file', 'cavatica_source_file',
+                    'cavatica_app', 'bs_id', 'cavatica_source_path',
+                    'cavatica_task']
+
+        # Make sure the required tags are there
+        for tag in req_tags:
+            if tag not in tags:
+                return 'missing required tag {}'.format(tag)
+
+        gf = self.new_file(bucket, key, record['s3']['object']['eTag'],
+                           record['s3']['object']['size'], gf_id=gf_id)
+
+        # Update tags with gf_id if it wasn't already in the tags
+        if gf_id is None:
+            tags['gf_id'] = gf['kf_id']
+            tagset = {'TagSet': [{'Key': k, 'Value': v} for k, v in tags.items()]}
+            r = s3.put_object_tagging(Bucket=bucket, Key=key, Tagging=tagset)
+
+        return tags
 
 
-def process_record(api, record):
-    """
-    Imports a single file from an s3 event into the dataservice
+    def new_file(self, bucket, key, etag, size,
+                 gf_id=None):
+        """
+        Creates a new genomic file in the dataservice
 
-    The object in the event must already be tagged with the following 
-    required fields in order to be imported:
+        :param bucket: The bucket of the object
+        :param key: The key of the object
+        :param etag: The ETag of the object
+        :param size: The size in bytes of the object
+        :param gf_id: Optional kf_id for the genomic file
+        """
+        file_name = key.split('/')[-1]
+        hashes = {'etag': etag.replace('"', '')}
+        urls = ['s3://{}/{}'.format(bucket, key)]
+        file_format = key.split('/')[-1].lower()
+        file_format = file_format[file_format.find('.')+1:]
+        data_type = DATA_TYPES[file_format]
+        harmonized = key.startswith('harmonized/')
 
-    - `cavatica_harmonized_file`
-    - `cavatica_source_file`
-    - `cavatica_app`
-    - `bs_id`
-    - `cavatica_source_path`
-    - `cavatica_task`
+        gf = {
+            'file_name': file_name,
+            'file_format': file_format,
+            'data_type': data_type,
+            'availability': 'Immediate Download',
+            'controlled_access': True,
+            'is_harmonized': harmonized,
+            'hashes': hashes,
+            'size': size,
+            'urls': urls
+        }
 
-    If there is a `gf_id` tag on the object already, check to see if that
-    file already exists in the dataservice, if it does not, assume that
-    it is a pre-determined kf_id and use it when creating a new genomic file.
-    
-    Once the genomic file has been imported to the dataservice, tag the object
-    with the kf_id under the `gf_id` tag, unless there was already a `gf_id`
-    field there.
-    """
-    bucket = record['s3']['bucket']['name']
-    key = record['s3']['object']['key']
+        if gf_id:
+            gf['kf_id'] = gf_id
 
-    tags = s3.get_object_tagging(Bucket=bucket, Key=key)
-    tags = {t['Key']: t['Value'] for t in tags['TagSet']}
+        resp = requests.post(self.api+'genomic-files', json=gf)
 
-    # Skip if there is a kf_id assigned already and exists in dataservice
-    gf_id = check_gf_id(tags)
+        if (resp.status_code != 201 or
+            'results' not in resp.json() or
+            'kf_id' not in resp.json()['results']):
+            raise DataServiceException('bad dataservice response')
 
-    req_tags = ['cavatica_harmonized_file', 'cavatica_source_file',
-                'cavatica_app', 'bs_id', 'cavatica_source_path',
-                'cavatica_task']
+        return resp.json()['results']
 
-    # Make sure the required tags are there
-    for tag in req_tags:
-        if tag not in tags:
-            return 'missing required tag {}'.format(tag)
+    def get_gf_id_tag(self, tags):
+        """
+        Returns a gf_id after verifying that it exists in list of tags
+        and checking that it does not yet exist in the dataservice.
 
-    file_name = key.split('/')[-1]
-    hashes = {'etag': record['s3']['object']['eTag']}
-    size = record['s3']['object']['size']
-    urls = ['s3://{}/{}'.format(bucket, key)]
-    file_format = key.split('/')[-1].lower()
-    file_format = file_format[file_format.find('.')+1:]
-    data_type = DATA_TYPES[file_format]
+        If there is a `gf_id` tag in the tagset, try to look up that kf_id
+        in the dataservice. If the dataservice does not return 404, assume
+        the genomic file has already been imported and raise an exception.
 
-    gf = {
-        'file_name': file_name,
-        'file_format': file_format,
-        'data_type': data_type,
-        'availability': 'Immediate Download',
-        'controlled_access': True,
-        'is_harmonized': True,
-        'hashes': hashes,
-        'size': size,
-        'urls': urls
-    }
-    # There is already a kf_id tagged on the file, so use that
-    if gf_id is not None:
-        gf['kf_id'] = gf_id
-
-    resp = requests.post(api+'genomic-files', json=gf)
-
-    if ('results' not in resp.json() and
-        'kf_id' not in resp.json()['results']):
-        return 'bad response from dataservice'
-
-    # Update tags with gf_id if it wasn't already in the tags
-    if gf_id is None:
-        tags['gf_id'] = resp.json()['results']['kf_id']
-        tagset = {'TagSet': [{'Key': k, 'Value': v} for k, v in tags.items()]}
-        r = s3.put_object_tagging(Bucket=bucket, Key=key, Tagging=tagset)
-
-    res = register_input(api, source_path)
-
-    return {'harmonized': 'imported', 'source': res}
+        :param tags: The tags on the object as a {name: value} dict
+        :returns: a kf_id of a genomic file, if the tagset contains a `gf_id`
+            tag with a kf_id that does not exist in the dataservice,
+            `None` otherwise
+        :raises: `ImportException` if a file with the matching kf_id already
+            exists in the dataservice
+        """
+        gf_id = None
+        if 'gf_id' in tags:
+            url = self.api+'genomic-files/'+tags['gf_id']
+            resp = requests.get(url)
+            if resp.status_code != 404 and 'results' in resp.json():
+                raise ImportException(tags['gf_id'] + ' already registered')
+            # Save for later so we can import with pre-determined id
+            gf_id = tags['gf_id']
+        return gf_id
 
 
-def register_input(api, source_path):
-    """
-    Registers a source genomic file given an s3 path
-    """
-    bucket = source_path.replace('s3://', '').split('_')[0]
-    key = '/'.join(source_path.split('/')[1:])
+    def register_input(self, tags):
+        """
+        Registers a source genomic file given an s3 path
+        """
+        source_path = tags['cavatica_source_path']
+        bucket = source_path.replace('s3://', '').split('/')[0]
+        key = '/'.join(source_path.split('/')[1:])
 
-    tags = s3.get_object_tagging(Bucket=bucket, Key=key)
-    tags = {t['Key']: t['Value'] for t in tags['TagSet']}
 
-    gf_id = check_gf_id(tags)
-    if gf_id:
-        return 'source file already registered'
+        tags = s3.get_object_tagging(Bucket=bucket, Key=key)
+        tags = {t['Key']: t['Value'] for t in tags['TagSet']}
 
-    obj = s3.get_object(Bucket=bucket, Key=key)
-    
-    file_name = key.split('/')[-1]
-    hashes = {'etag': ob['ETag']}
-    size = obj['ContentLength']
-    urls = ['s3://{}/{}'.format(bucket, key)]
-    file_format = key.split('/')[-1].lower()
-    file_format = file_format[file_format.find('.'):]
-    data_type = DATA_TYPES[file_format]
+        gf_id = self.get_gf_id_tag(tags)
 
-    gf = {
-        'file_name': file_name,
-        'file_format': file_format,
-        'data_type': data_type,
-        'availability': 'Immediate Download',
-        'controlled_access': True,
-        'is_harmonized': False,
-        'hashes': hashes,
-        'size': size,
-        'urls': urls
-    }
+        obj = s3.get_object(Bucket=bucket, Key=key)
+        
+        gf = self.new_file(bucket, key, obj['ETag'], obj['ContentLength'])
 
-    resp = requests.post(api+'genomic-files', json=gf)
-
-    if ('results' not in resp.json() and
-        'kf_id' not in resp.json()['results']):
-        return 'bad response from dataservice'
-
-    # Update tags with gf_id if it wasn't already in the tags
-    if gf_id is None:
-        tags['gf_id'] = resp.json()['results']['kf_id']
-        tagset = {'TagSet': [{'Key': k, 'Value': v} for k, v in tags.items()]}
-        r = s3.put_object_tagging(Bucket=bucket, Key=key, Tagging=tagset)
-
-    return 'imported'
+        # Update tags with gf_id if it wasn't already in the tags
+        if gf_id is None:
+            tags['gf_id'] = gf['kf_id']
+            tagset = {'TagSet': [{'Key': k, 'Value': v} for k, v in tags.items()]}
+            r = s3.put_object_tagging(Bucket=bucket, Key=key, Tagging=tagset)
--- tests/test_service.py
+++ tests/test_service.py
@@ -9,6 +9,9 @@ import service
 BUCKET = 'kf-study-us-east-1-dev-sd-9pyzahhe'
 OBJECT = 'harmonized/cram/60d33dec-98db-446c-ac64-f4d027588f26.cram'
 
+SOURCE_BUCKET = 'kf-seq-data-washu'
+SOURCE_OBJECT = 'OrofacialCleft/bd042b24ae844a57ace28cf70cb3c852.bam'
+
 TAGS = {
     'TagSet': [
         {
@@ -39,10 +42,11 @@ TAGS = {
 }
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope='function')
 def obj():
     @mock_s3
     def with_obj():
+        """ Create a harmonized file and its source file """
         s3 = boto3.client('s3')
         b = s3.create_bucket(Bucket=BUCKET)
         ob = s3.put_object(Bucket=BUCKET, Key=OBJECT, Body=b'test')
@@ -50,6 +54,11 @@ def obj():
         response = s3.put_object_tagging(
             Bucket=BUCKET, Key=OBJECT, Tagging=TAGS
         )
+
+        source_b = s3.create_bucket(Bucket=SOURCE_BUCKET)
+        source_ob = s3.put_object(Bucket=SOURCE_BUCKET, Key=SOURCE_OBJECT,
+                                  Body=b'test')
+
         return ob
     return with_obj
 
@@ -70,12 +79,22 @@ def test_create(event, obj):
     req = mock.start()
     mock_resp = MagicMock()
     mock_resp.json.return_value = {'results': {'kf_id': 'GF_00000000'}}
+    mock_resp.status_code = 201
     req.post.return_value = mock_resp
 
     s3 = boto3.client('s3')
 
-    service.handler(event, {})
+    res = service.handler(event, {})
 
+    assert len(res) == 1
+    k = '{}/{}'.format(BUCKET, OBJECT)
+    assert res[k]['source'] == 'imported'
+    assert res[k]['harmonized'] == 'imported'
+
+    # Should be called once for the harmonized file, once for the source file
+    assert req.post.call_count == 2
+
+    # Check harmonized file call
     expected = {
         'file_name': '60d33dec-98db-446c-ac64-f4d027588f26.cram',
         'file_format': 'cram',
@@ -87,11 +106,29 @@ def test_create(event, obj):
         'size': 1024,
         'urls': ['s3://{}/{}'.format(BUCKET, OBJECT)]
     }
+    req.post.assert_any_call('http://api.com/genomic-files', json=expected)
 
-    assert req.post.call_count == 1
-    req.post.assert_called_with('http://api.com/genomic-files', json=expected)
+    # Check source file call
+    expected = {
+        'file_name': 'bd042b24ae844a57ace28cf70cb3c852.bam',
+        'file_format': 'bam',
+        'data_type': 'Aligned Reads',
+        'controlled_access': True,
+        'availability': 'Immediate Download',
+        'is_harmonized': False,
+        'hashes': {'etag': '098f6bcd4621d373cade4e832627b4f6'},
+        'size': 4,
+        'urls': ['s3://{}/{}'.format(SOURCE_BUCKET, SOURCE_OBJECT)]
+    }
+    req.post.assert_any_call('http://api.com/genomic-files', json=expected)
 
+    # Check that the harmonized file has been updated with the new kf_id
     response = s3.get_object_tagging(Bucket=BUCKET, Key=OBJECT)
+    tags = {t['Key']: t['Value'] for t in response['TagSet']}
+    assert 'gf_id' in tags
+
+    # Check that the source file has been updated with the new kf_id
+    response = s3.get_object_tagging(Bucket=SOURCE_BUCKET, Key=SOURCE_OBJECT)
     tags = {t['Key']: t['Value'] for t in response['TagSet']}
     assert 'gf_id' in tags
 
@@ -165,3 +202,86 @@ def test_existing_gf_id(event, obj):
     assert req.post.call_count == 1
 
     mock.stop()
+
+
+def test_new_file():
+    """ Test that new file are created correctly """
+    os.environ['DATASERVICE_API'] = 'http://api.com/'
+    mock = patch('service.requests')
+    req = mock.start()
+    mock_resp = MagicMock()
+    mock_resp.status_code = 201
+    mock_resp.json.return_value = {'results': {'kf_id': 'GF_00000000'}}
+    req.post.return_value = mock_resp
+
+    importer = service.FileImporter('http://api.com/', 'abc123')
+    
+    res = importer.new_file(BUCKET, OBJECT,
+                            'd41d8cd98f00b204e9800998ecf8427e', 1024)
+
+    expected = {
+        'file_name': '60d33dec-98db-446c-ac64-f4d027588f26.cram',
+        'file_format': 'cram',
+        'data_type': 'Aligned Reads',
+        'controlled_access': True,
+        'availability': 'Immediate Download',
+        'is_harmonized': True,
+        'hashes': {'etag': 'd41d8cd98f00b204e9800998ecf8427e'},
+        'size': 1024,
+        'urls': ['s3://{}/{}'.format(BUCKET, OBJECT)]
+    }
+
+    req.post.assert_called_with('http://api.com/genomic-files', json=expected)
+    assert req.post.call_count == 1
+
+
+def test_new_file_gf_id():
+    """ Test that new file with predefined kf_id """
+    os.environ['DATASERVICE_API'] = 'http://api.com/'
+    mock = patch('service.requests')
+    req = mock.start()
+    mock_resp = MagicMock()
+    mock_resp.status_code = 201
+    mock_resp.json.return_value = {'results': {'kf_id': 'GF_00000001'}}
+    req.post.return_value = mock_resp
+
+    importer = service.FileImporter('http://api.com/', 'abc123')
+    
+    res = importer.new_file(BUCKET, OBJECT,
+                            'd41d8cd98f00b204e9800998ecf8427e', 1024,
+                            gf_id='GF_00000001')
+
+    expected = {
+        'kf_id': 'GF_00000001',
+        'file_name': '60d33dec-98db-446c-ac64-f4d027588f26.cram',
+        'file_format': 'cram',
+        'data_type': 'Aligned Reads',
+        'controlled_access': True,
+        'availability': 'Immediate Download',
+        'is_harmonized': True,
+        'hashes': {'etag': 'd41d8cd98f00b204e9800998ecf8427e'},
+        'size': 1024,
+        'urls': ['s3://{}/{}'.format(BUCKET, OBJECT)]
+    }
+
+    req.post.assert_called_with('http://api.com/genomic-files', json=expected)
+    assert req.post.call_count == 1
+
+
+def test_new_file_error():
+    """ Test that new file with predefined kf_id """
+    os.environ['DATASERVICE_API'] = 'http://api.com/'
+    mock = patch('service.requests')
+    req = mock.start()
+    mock_resp = MagicMock()
+    mock_resp.status_code = 400
+    mock_resp.json.return_value = {'results': {'kf_id': 'GF_00000002'}}
+    req.post.return_value = mock_resp
+
+    importer = service.FileImporter('http://api.com/', 'abc123')
+    
+    with pytest.raises(service.DataServiceException):
+        res = importer.new_file(BUCKET, OBJECT,
+                                'd41d8cd98f00b204e9800998ecf8427e', 1024)
+
+    assert req.post.call_count == 1
